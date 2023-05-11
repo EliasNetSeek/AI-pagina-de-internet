711c17910f637065e1414b7a5151f93986df641f
EliasNetSeek <50449446+EliasNetSeek@users.noreply.github.com>
2023-05-11 16:32:33 +0000

    dynamic placement and random dealt cards

---
+++ src/js/cards.js
@@ -0,0 +1,113 @@
+var cardList = [
+    {
+        number: 1,
+        name: "THE EMPEROR",
+    },
+    {
+        number: 2,
+        name: "THE HANGED",
+    },
+    {
+        number: 3,
+        name: "THE SUN",
+    },
+    {
+        number: 4,
+        name: "THE STAR",
+    },
+    {
+        number: 5,
+        name: "THE MAGICIAN",
+    },
+    {
+        number: 6,
+        name: "THE TOWER",
+    },
+    {
+        number: 7,
+        name: "THE HIEROPHANT",
+    },
+    {
+        number: 8,
+        name: "X CUPS",
+    },
+    {
+        number: 9,
+        name: "II SWORDS",
+    },
+    {
+        number: 10,
+        name: "ACE CUPS",
+    },
+    {
+        number: 11,
+        name: "II CUPS",
+    },
+    {
+        number: 12,
+        name: "WANDS",
+    },
+    {
+        number: 13,
+        name: "QUEEN SWORDS",
+    },
+    {
+        number: 14,
+        name: "PENTACLES",
+    },
+]
+
+var cardPosition = [
+    {
+        top: 300,
+        left: 100,
+        rotate: -20,
+    },
+    {
+        top: 200,
+        left: 440,
+        rotate: -10,
+    },
+    {
+        top: 170,
+        left: 780,
+        rotate: 0,
+    },
+    {
+        top: 200,
+        left: 1120,
+        rotate: 10,
+    },
+    {
+        top: 300,
+        left: 1460,
+        rotate: 20,
+    },
+]
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max + 1);
+}
+
+window.onload = function(){
+    var deltCards = [];
+    for (let i = 0; i < 5; i++) {
+        var kard = getRandomInt(cardList.length);
+
+        if (!deltCards.includes(kard)) {
+            deltCards.push(kard);
+        } else {
+            kard = getRandomInt(cardList.length);
+        }
+
+        document.getElementById('card' + (i+1) + 'face').style.background = 'url(/src/img/cards/Card' + kard +'.png)';
+        document.getElementById('card' + (i+1) + 'face').style.backgroundSize = '240px 430px';
+        document.getElementById('card' + (i+1)).style.top = cardPosition[i].top;
+        document.getElementById('card' + (i+1)).style.left = cardPosition[i].left;
+        document.getElementById('card' + (i+1)).style.transform = 'rotate('+cardPosition[i].rotate+'deg)';
+        console.log(i + ": " + cardList[kard-1].name)
+    }
+};
+
+console.log(cardList[1].number)
+console.log(getRandomInt(cardList.length));
