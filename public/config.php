7e271c8ffd0fd6a2a955aa1fefb7112cf5f411c0
EliasNetSeek <50449446+EliasNetSeek@users.noreply.github.com>
2023-05-25 16:33:14 +0000

    mysql parially added

---
+++ public/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST","sql8.freemysqlhosting.net");
+define("DB_USER","sql8621278");
+define("DB_PASSWORD","ps4mlw3JDj");
+define("DB_NAME","sql8621278");
+$mysqli = new mysqli(DB_HOST,DB_USER, DB_PASSWORD,DB_NAME);
+?>
